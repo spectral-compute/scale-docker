1d7cc03b8852c252498fdcef2fb2c256bba46c6c
Aria Shrimpton <aria@spectralcompute.co.uk>
2025-11-27 16:57:19 +0000

    Mount graphics cards in runValidation script

--- scripts/runValidation.sh
+++ scripts/runValidation.sh
@@ -15,4 +15,4 @@ fi
 
 SCALE_VALIDATION_BRANCH="${SCALE_VALIDATION_BRANCH:-}"
 
-$CONTAINER_COMMAND run -it --rm -e "SCALE_VALIDATION_BRANCH=$SCALE_VALIDATION_BRANCH" -v "$(dirname "$0")/validationEntrypoint.sh:/validationEntrypoint.sh" "$IMAGE" /validationEntrypoint.sh "$@"
+$CONTAINER_COMMAND run -it --rm --device /dev/kfd --device /dev/dri -e "SCALE_VALIDATION_BRANCH=$SCALE_VALIDATION_BRANCH" -v "$(dirname "$0")/validationEntrypoint.sh:/validationEntrypoint.sh" "$IMAGE" /validationEntrypoint.sh "$@"
