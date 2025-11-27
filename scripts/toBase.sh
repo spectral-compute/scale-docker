ea2c0d16cabd7e2f0db8f8418d26bcf0c8736a23
Aria Shrimpton <aria@spectralcompute.co.uk>
2025-11-27 11:46:20 +0000

    Actually remove things from the base/runtime images

--- scripts/toBase.sh
+++ scripts/toBase.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 # Transform the installed SCALE package to have what we want in the base package
+set -e
+TO_REMOVE=(
+    /opt/rocm-scale/include
+    /opt/rocm-scale/lib/cmake
+    /opt/rocm-scale/lib/pkgconfig
+    /opt/rocm-scale/share/doc
+    /opt/scale/include
+    /opt/scale/cccl
+    /opt/scale/lib
+    /opt/scale/llvm/share/man
+)
+rm -rf ${TO_REMOVE[@]}
--- scripts/toRuntime.sh
+++ scripts/toRuntime.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
 # Transform the installed SCALE package to have what we want in the runtime package
+set -e
+TO_REMOVE=(
+    /opt/rocm-scale/lib/librocblas*
+    /opt/rocm-scale/lib/rocblas
+    /opt/rocm-scale/lib/rocprim
+    /opt/rocm-scale/lib/librocfft*
+    /opt/rocm-scale/lib/rocfft
+    /opt/rocm-scale/lib/librocsolver*
+    /opt/rocm-scale/lib/rocsolver
+    /opt/rocm-scale/lib/librocsparse*
+    /opt/rocm-scale/lib/librocrand*
+    /opt/rocm-scale/lib/rocrand
+    /opt/rocm-scale/share/doc
+
+    /opt/scale/lib/libcudnn*
+    /opt/scale/lib/libcusparse*
+    /opt/scale/lib/libcusolver*
+    /opt/scale/lib/libcurand*
+    /opt/scale/lib/libcublas*
+    /opt/scale/lib/libcufft*
+)
+rm -rf ${TO_REMOVE[@]}
