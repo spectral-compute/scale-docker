e79b3af8fcc567185ba2b4fa45155fa3f4b2a4cc
Aria Shrimpton <aria@spectralcompute.co.uk>
2025-11-27 11:35:32 +0000

    Make all images a bit smaller by deleting unused stuff

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 # Cleanup leftover/unused files that we don't want to keep in our docker files
 # Mostly package list caches, etc.
+set -e
+
+TO_REMOVE=(
+    /var/lib/apt/lists /var/cache/* /var/log/* /scale-repos.*
+    # Because we currently have a hard dependency on amdgpu-dkms,
+    # we also end up installing that and a bunch of its build dependencies
+    # This isn't ideal, but for now just nuke it manually.
+   /boot  /etc/dkms /etc/fonts /usr/src /usr/lib/gcc /var/lib/dkms
+)
+rm -rf ${TO_REMOVE[@]}
