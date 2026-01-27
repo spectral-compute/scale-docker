2d19b64e389a0e79247923a691991d9885f3a0af
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-01-27 12:38:30 +0000

    Also push to docker.io

--- scripts/supported.sh
+++ scripts/supported.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 DOCKER_REGISTRY="${DOCKER_REGISTRY:-quay.io}"
-DOCKER_REPO=spectral-compute/scale
+if [ "$DOCKER_REGISTRY" == "docker.io" ]; then
+    DOCKER_REPO=spectralcompute/scale # docker.io
+    DOCKER_REPO2=spectral-compute/scale # everywhere else
+else
+    DOCKER_REPO=spectral-compute/scale # everywhere else
+    DOCKER_REPO2=spectralcompute/scale # docker.io
+fi
 
 SUPPORTED_CUDA_VERSIONS="13.0.2 12.1.0 11.8.0 11.4.3"
 SUPPORTED_DISTROS="ubuntu22.04 ubuntu24.04 rocky9"
