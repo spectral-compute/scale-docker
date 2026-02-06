678b350e707db9cbccc514a6f036760b2c6b5684
Jon Chesterfield <jon@spectralcompute.co.uk>
2026-02-06 16:58:12 +0000

    Accept Y|Yes as well as 1 for license variable

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "${SCALE_LICENSE_PRINT:-0}" == "1" ]; then
-    cat /opt/scale/LICENSE.txt
-fi
+case "${SCALE_LICENSE_PRINT:-0}" in
+    [Yy]|[Yy]es|1)
+        cat /opt/scale/LICENSE.txt
+        ;;
+    *)
+        ;;
+esac
 
-if [ "${SCALE_LICENSE_ACCEPT:-0}" != "1" ]; then
+case "${SCALE_LICENSE_ACCEPT:-0}" in
+    [Yy]|[Yy]es|1)
+        ;;
+    *)
     cat <<EOF
 
-    SCALE license aggreement
+    SCALE license agreement
     ========================
 
     In order to use this container image, you must accept the SCALE software license, a copy of which can be found at:
@@ -20,11 +27,10 @@ if [ "${SCALE_LICENSE_ACCEPT:-0}" != "1" ]; then
     To print out the license text in full, set SCALE_LICENSE_PRINT=1
 
 EOF
-
     exit 1
-fi
+esac
 
-echo "EULA accepted via environment variable (SCALE_LICENSE_ACCEPT=1)."
+echo "EULA accepted via environment variable (SCALE_LICENSE_ACCEPT=${SCALE_LICENSE_ACCEPT})."
 if [[ $# -eq 0 ]]; then
     exec "/bin/bash"
 else
